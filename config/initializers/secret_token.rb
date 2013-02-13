172f1854cd05c0b7890d9b705a828554f06e1f12
Felipe Amaral de Mattos <felipeamaraldemattos@gmail.com>
2013-02-12 23:13:14 -0500

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudiareApp::Application.config.secret_token = '001f5b1beab952f93a2979d5c7ec64b1ac39a971f736a87251735fd5f86e500cdb5f6f947bce3e19a41597b1a13fcebb1059599530f1ba63da4bd92db15fe86a'
