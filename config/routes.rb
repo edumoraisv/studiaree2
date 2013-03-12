bdd837280ad116feb1a0280ebfef6912877765d0
Felipe Amaral de Mattos <felipeamaraldemattos@gmail.com>
2013-03-11 20:30:16 -0400

    Add user following v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 StudiareApp::Application.routes.draw do
   
-  resources :users
-  
   root to: 'static_pages#home'
-  resources :users
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+ 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  
 
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
